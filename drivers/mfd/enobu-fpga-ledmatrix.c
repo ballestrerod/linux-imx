c064a87dfab19b66d8be79ad404873ae28864d2d
Sergio Biasi <sergio.biasi@leonardocompany.com>
2020-11-26 18:34:52 +0100

    Changed another log level

--- drivers/mfd/enobu-fpga-ledmatrix.c
+++ drivers/mfd/enobu-fpga-ledmatrix.c
@@ -959,7 +959,7 @@ int matrixdisp_char(struct enobu_fpga *enobufpga, const char *dchr)
     matrixdisp_write_mode(enobufpga, MATRIXDISP_MODE_FIXED);
     matrixdisp_chr_display(enobufpga, dchr);
 
-    printk(KERN_ERR "[MATRIX-CHAR] %s\n", dchr);
+    printk(KERN_INFO "[MATRIX-CHAR] '%s'\n", dchr);
 
     return 0;
 }
@@ -1042,7 +1042,7 @@ int matrixdisp_prnt(struct enobu_fpga *enobufpga, const char *dstr)
     matrixdisp_write_mode(enobufpga, MATRIXDISP_MODE_SCROLL);
     matrixdisp_str_display(enobufpga, dstr);
 
-    printk(KERN_INFO "[MATRIX-PRINT] %s\n", dstr);
+    printk(KERN_INFO "[MATRIX-PRINT] '%s'\n", dstr);
 
     return 0;
 }
